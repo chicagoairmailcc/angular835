b209036ecbd1837bd1e11604469e4191ef7a8fa9
Level 3 <level3@tildeteam.org>
2019-09-22 10:45:18 -0600

    add a new service and component for quiz

---
+++ src/app/quiz.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, Inject, Output, EventEmitter } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Item } from './item';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+  constructor(private http: HttpClient) { }
+
+  quizUrl = '/assets/quiz/quiz.json';
+
+  get() {
+    return this.http.get<any>(this.quizUrl)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      );
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
