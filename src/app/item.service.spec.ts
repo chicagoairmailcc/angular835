252280f2e5ba669b6264ed177f3f732dffd65e74
Level 3 <level3@tildeteam.org>
2019-09-30 16:55:54 -0600

    test get method

--- src/app/item.service.spec.ts
+++ src/app/item.service.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ItemService } from './item.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,7 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 describe('ItemService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
-      HttpClientModule
+      HttpClientModule,
+      HttpClientTestingModule
     ]
   }));
 
@@ -14,4 +16,21 @@ describe('ItemService', () => {
     const service: ItemService = TestBed.get(ItemService);
     expect(service).toBeTruthy();
   });
+
+  it('should return a valid item',
+    inject(
+      [HttpTestingController, ItemService],
+      (httpMock: HttpTestingController, service: ItemService) => {
+        const inputString = '1';
+        service.getItem(inputString).subscribe(data => {
+          expect(data.id).toBe('1');
+        });
+        const request = httpMock.expectOne('/assets/item/1.json');
+        expect(request.request.method).toEqual('GET');
+        request.flush({
+          id: '1',
+          name: 'item one',
+          description: 'This actually works'
+        });
+      }));
 });
